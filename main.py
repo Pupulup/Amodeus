5c807b2b3ce6eb42a6b7b9234721cf438578b55f
Osuuspankk1 <101213195+Pupulup@users.noreply.github.com>
2023-04-16 02:30:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,259 @@
-import openai
-from telegram.ext import Updater, MessageHandler
+import kb as kb
+import json
+import telebot
+import requests as req
+import os
+import logging
+import random
+import time
+import re
+from dataclasses import dataclass
+from bs4 import BeautifulSoup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import Message
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from bs4 import BeautifulSoup
+import requests
+from telebot import TeleBot, types
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import requests
+from secret_info import API_TOKEN, vafli_group_id, open_weather_token
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 
-API_TOKEN = '6012611381:AAEksVc4WZ-LEPEgjElNSFbi29osua_ir8Y'
-openai_api_key = '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'
+# ПИНГ БОТА
+@dp.message_handler(commands=['p'])
+async def ping(message: types.Message):
+    await message.reply("Я тут")
 
 
-def ask_gpt(text):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=text,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    message = response.choices[0].text.strip()
-    return message
+@dp.message_handler(commands=['all'])
+async def all(message: types.Message):
+    await message.answer("@Osuuspankk1 @Marrolk @dashe4kin @yayauuyaya")
 
 
-def echo(update, context):
-    response = ask_gpt(update.message.text)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+@dp.message_handler(commands=['h'])
+async def helper(message: types.Message):
+    await message.answer("/p - пингует бота(чтобы проверять что он точно лежит, а не проблема в коде где-то \n"
+                         "/all - тегает всех(в стадии доработки)\n"
+                         "/gor - гороскоп")
 
 
-updater = Updater(token=API_TOKEN, use_context=True)
-updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-updater.start_polling()
-updater.idle()
+# ПОГОДА СПБ
+
+type_weather = {
+    "Clear": "Ясно",
+    "Clouds": "Облачно",
+    "Rain": "Дождь",
+    "Drizzle": "Дождь",
+    "Thunderstorm": "Гроза",
+    "Snow": "Снег ",
+    "Mist": "Туман"
+}
+
+# СТАТИСТИКА
+egor, misha, olga, dascha = 0, 0, 0, 0
+
+# ГОРОСКОПЧИК
+with open("first.txt", "r", encoding="utf-8") as f1:
+    first = f1.readlines()
+with open("second.txt", "r", encoding="utf-8") as f2:
+    second = f2.readlines()
+with open("second_add.txt", "r", encoding="utf-8") as f2_add:
+    second_add = f2_add.readlines()
+with open("third.txt", "r", encoding="utf-8") as f3:
+    third = f3.readlines()
+
+
+@dp.message_handler(commands=['gor'])
+async def get_text_messages(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['♈️ Овен ♈️', '♉️ Телец ♉️', '♊️ Близнецы ;3 ♊️', '♋️ Рак ♋️', '♌️ Лев ♌️', '♍️ Дева ♍️', '♎️ Весы ♎️',
+               '♏️ Скорпион ♏️', '♐️ Стрелец ♐️', '♑️ Козерог ♑️', '♒️ Водолей ♒️', '♓️ Рыбы ♓️']
+    keyboard.add(*buttons)
+    await message.answer('Я, конечно, в такое не верю, но дело твое', reply_markup=keyboard)
+
+
+@dp.message_handler(lambda message: message.text == "♈️ Овен ♈️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♉️ Телец ♉️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♊️ Близнецы ;3 ♊️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♋️ Рак ♋️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♌️ Лев ♌️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♍️ Дева ♍️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♎️ Весы ♎️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♏️ Скорпион ♏️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♐️ Стрелец ♐️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♑️ Козерог ♑️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♒️ Водолей ♒️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text == "♓️ Рыбы ♓️")
+async def oven(message: types.Message):
+    msg = random.choice(first) + '' + random.choice(second) + '' + random.choice(second_add) + '' + random.choice(third)
+    await message.reply(msg, reply_markup=types.ReplyKeyboardRemove())
+
+
+# КУРИСУ ОТВЕЧАЕТ)
+@dp.message_handler()
+async def message_from_users(message: Message):
+    global egor, misha, olga, dascha
+    if message.from_user.id == '1330610087':
+        egor += 1
+    if message.from_user.id == '1766360104':
+        misha += 1
+    if message.from_user.id == '1071828913':
+        olga += 1
+    if message.from_user.id == '313658535':
+        dascha += 1
+    dn = ["доброй ночи", "я спать", "спокойной ночи", "сладких снов"]
+    mat = ["блять", "сука", "пидор", "пидарас", "пидорас", "бля", "хуй", "пизда", "пиздец", "хуйня", "ебать", "заебало",
+           "нахуй", "шлюха", "хуле", "cхуяле", "kurva", "курва", 'хуям', 'ебало', 'спизил']
+    nemat = ['ублюдок']
+    # vanya = ["ваня", "иван"]
+    buhaem = ["бухаем", "бухаем?", "пьем?", "в кб?", "в кб", "бухать", "пить", "в бар"]
+    shutka = ["(шутка)"]
+    tilt = ["я в тильте"]
+    radost = ["ура!"]
+    okno = ["я в окно", "в окно"]
+    goroskop = ["гороско курису "]
+    if any(item in message.text.lower() for item in dn):
+        await message.reply("Добрых снов)")
+    elif any(item in message.text.lower() for item in mat):
+        await message.reply("Отжимайся!")
+    elif any(item in message.text.lower() for item in nemat):
+        await message.reply("Ублюдок - это не мат")
+    elif any(item in message.text.lower() for item in buhaem):
+        await message.reply("ДА!")
+    elif any(item in message.text.lower() for item in shutka):
+        await message.reply("Не шути больше, у тебя плохо получается")
+    elif any(item in message.text.lower() for item in tilt):
+        await bot.send_sticker(message.chat.id,
+                               "CAACAgUAAxkBAAEIaXNkKEMkiMwJeI0QwszhpMwnPNzqTwACKgkAAnQr4AVa1q3UYDBfOy8E")
+    elif any(item in message.text.lower() for item in goroskop):
+        await message.reply("Я, конечно, в такое не верю, но дело твое")
+    elif any(item in message.text.lower() for item in radost):
+        await bot.send_sticker(message.chat.id,
+                               "CAACAgUAAxkBAAEIaXpkKESFBPmv-v0AAcm55_ZV2Nq-3oQAAvgIAAJ0K-AFpECLSEAufFEvBA")
+    elif any(item in message.text.lower() for item in okno):
+        await bot.send_sticker(message.chat.id,
+                               "CAACAgUAAxkBAAEIaZVkKEw8r1wQUN5GzDKnURKzJdjJUwACCQkAAnQr4AUYavnANwt75y8E")
+
+    # elif any(item in message.text.lower() for item in vanya):
+    #   await message.reply("сдохни")
+
+    # ПОГОДА
+    try:
+        weather = ["погода", "спб"]
+        if any(item in message.text.lower() for item in weather):
+            message.text = "Санкт-Петербург"
+        else:
+            message.text = "hbbfg"
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+
+        temp = data['main']['temp']
+
+        if data["weather"][0]["main"] in type_weather:
+            wd = type_weather[data["weather"][0]["main"]]
+        else:
+            wd = ""
+        if wd == 'Дождь':
+            umbrl = 'и возьмите зонт'
+        else:
+            umbrl = ''
+        if temp < -25:
+            result = 'Там так холодно, лучше останься дома и попей чай'
+        if temp < 1:
+            result = 'Холодно, одевайся потеплее'
+        elif temp < 12:
+            result = 'Прохладно'
+        elif temp < 25:
+            result = 'Тепло'
+        else:
+            result = 'Жарко'
+
+        await message.reply(f"{result} {umbrl} ({data['main']['temp']}C° {wd})")
+
+    finally:
+        return
+
+
+'''
+async def dolgi_timer(bot: Bot):
+    start_date = datetime.now()
+    flag = False
+    while True:
+        if datetime.now().hour == start_date.hour:
+            flag = False
+        r = random.randint(0, 25)
+        if r == 0 and not flag:
+            await message_sender('@Osuuspankk1, закрывай долги', group_id, bot)
+            flag = True
+        await asyncio.sleep(3600)
+'''
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
